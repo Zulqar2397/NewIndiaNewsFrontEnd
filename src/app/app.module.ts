f2ef06f8d79294ddfd3c758263c8758706972903
pradyumna448 <pradyumna448@gmail.com>
2020-07-13 23:18:34 +0530

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutUsComponent } from './core/about-us/about-us.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { NewsComponent } from './core/news/news.component';
+import { NewsDetailsComponent } from './core/news-details/news-details.component';
+import { ContactUsComponent } from './core/contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    NewsComponent,
+    NewsDetailsComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
